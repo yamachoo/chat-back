66f21e04cde3289c419bab97d44052aba8793014
yamachoo <51016095+yamachoo@users.noreply.github.com>
2021-01-14 22:57:15 +0900

    fix user type

--- src/database/index.ts
+++ src/database/index.ts
@@ -3,8 +3,8 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 type user = {
-  name: String,
-  email: String
+  name: string,
+  email: string
 }
 
 export async function createUser (query: user): Promise<void> {
