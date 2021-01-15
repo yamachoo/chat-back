bf5e146dd32b678605c3a8b7e5b8a181fdadea10
yamachoo <51016095+yamachoo@users.noreply.github.com>
2021-01-15 19:23:22 +0900

    add register, login, logout

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import path from 'path'
 import cookieParser from 'cookie-parser'
 import logger from 'morgan'
+import passport from 'passport'
 
 import indexRouter from './routes/index'
 import usersRouter from './routes/users'
@@ -12,6 +13,8 @@ app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
+app.use(passport.initialize())
+app.use(passport.session())
 app.use(express.static(path.join(__dirname, '../public')))
 
 app.use('/', indexRouter)
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,67 @@
-import express from 'express'
+import express, { Express } from 'express'
+import passport from 'passport'
+import { Strategy as LocalStrategy } from 'passport-local'
+import bcrypt from 'bcrypt'
+import {
+  createUser,
+  readUsers
+} from '../database/users'
 
 const router = express.Router()
 
-/* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' })
+passport.use(new LocalStrategy({
+  usernameField: 'email'
+},
+async (email, password, done) => {
+  const users = await readUsers()
+  const user = users.find(user => user.email === email)
+
+  if (!user) {
+    return done(null, false, { message: 'Incorrect email.' })
+  }
+
+  const isValid = await bcrypt.compare(password, user.password)
+
+  if (!isValid) {
+    return done(null, false, { message: 'Incorrect password.' })
+  }
+
+  return done(null, user)
+}
+))
+passport.serializeUser((user: Express.User, done) => {
+  done(null, user)
 })
+passport.deserializeUser((user: Express.User, done) => {
+  done(null, user)
+})
+
+router.post('/register',
+  async (req, res) => {
+    const user = {
+      name: req.body.name,
+      email: req.body.email,
+      password: await bcrypt.hash(req.body.password, 10)
+    }
+
+    await createUser(user)
+
+    res.send('Register success!')
+  }
+)
+
+router.post('/login',
+  passport.authenticate('local'),
+  (req, res) => {
+    res.send('Login success!')
+  }
+)
+
+router.get('/logout',
+  (req, res) => {
+    req.logout()
+    res.send('Logout success!')
+  }
+)
 
 export default router
