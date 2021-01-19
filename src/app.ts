5d27f7d512187ee5582ff1f3a50c13a44086d606
yamachoo <51016095+yamachoo@users.noreply.github.com>
2021-01-19 20:53:55 +0900

    clean bin/www

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
+import http from 'http'
 import path from 'path'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
+import debug from 'debug'
 import express from 'express'
 import logger from 'morgan'
 import passport from 'passport'
@@ -8,10 +10,14 @@ import {
   ENDPOINT_PATH,
   ENDPOINT_VERSION
 } from './constants'
+import { normalizePort } from './helper'
 import indexRouter from './routes/index'
 import usersRouter from './routes/users'
 
 const app = express()
+const server = http.createServer(app)
+const deg = debug('chat-back:server')
+const port = normalizePort(process.env.PORT || '3000')
 const ENDPOINT = '/'.concat(ENDPOINT_PATH, '/', ENDPOINT_VERSION)
 
 app.use(logger('dev'))
@@ -26,4 +32,37 @@ app.use(cors())
 app.use(ENDPOINT.concat('/'), indexRouter)
 app.use(ENDPOINT.concat('/users'), usersRouter)
 
-export default app
+app.set('port', port)
+
+const onError = (error: NodeJS.ErrnoException) => {
+  if (error.syscall !== 'listen') {
+    throw error
+  }
+
+  const bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port
+
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges')
+      process.exit(1)
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use')
+      process.exit(1)
+    default:
+      throw error
+  }
+}
+
+const onListening = () => {
+  const addr = server.address()
+  const bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr?.port
+  deg('Listening on ' + bind)
+}
+
+server.listen(port)
+server.on('error', onError)
+server.on('listening', onListening)
